d3ded5353d68b8640128bc411fc594896a61b9db
Pranav Rayudu <tarakapranav@gmail.com>
2017-03-21 20:44:28 -0500

    Re-did backup method. Project completely "Physicified", but Raycast glitch remains
    Raycast returns 2 hits on the same frame (also proved through debug
    lines)
    Raycast "doubling" might be happening becuase forces are being added and
    things are moving in another script attatched to object. PHYSICS Raycast
    might also be called there as well. (problems between transform changes
    and physics might also be occuring)

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,72 +2,41 @@
 
 public class Controller : MonoBehaviour {
     Rigidbody rb;
-    //check 1
+    Vector3 input;
 
-    public float gravity = 9.81f;
+    public float gravity;
+    public float jumpForce;
+    public float moveForce;
 
-    public float mass = 0.3f;
-    public float moveAcceleration = 1f;
-    public float maxMoveSpeed = 2.0f;
-    public float slowdownTime = 0.3f;
+    void processMove () {
+        input.Set(Input.GetAxis("Horizontal") , 0 , Input.GetAxis("Vertical"));
 
-    public float jumpForce = 20;
-    public float jumpReact = 5;
+        //turn object around withought rotating camera
+        Transform mainCamera = Camera.main.transform;      
+        Vector3 diff = ( mainCamera.up * input.z + mainCamera.right * input.x ).normalized;
+        diff = transform.InverseTransformDirection(diff);
+        diff.y = 0;
+        diff = transform.TransformDirection(diff);
+        transform.LookAt(transform.position + diff , transform.up);
 
-    public float jumpTimeMargin = 0.1f;
-
-    private float moveSpeed = 0f;
-
-    private float lastInput;
-
-    private bool elevating = false;
-
-    //debug
-    private Vector3 lastDiff;
-
-    void Start () {
-        lastInput = Time.time;
-        lastDiff = Vector3.zero;
-
-        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * moveForce * Mathf.Clamp01(input.magnitude));
     }
 
-    void addGravity () {
-        rb.AddForce(-transform.up * gravity);
+    void useGravity () {
+        rb.AddForce(-transform.up * gravity * rb.mass);
     }
 
     void jump () {
-        rb.AddForce(transform.up * gravity * jumpForce);
+        rb.AddForce(transform.up * jumpForce);
     }
 
-    void processMove () {
-        float zAxis = Input.GetAxis("Vertical");
-        float xAxis = Input.GetAxis("Horizontal");
-
-        moveSpeed += ( Mathf.Abs(xAxis) + Mathf.Abs(zAxis) ) * moveAcceleration * Time.deltaTime;
-        moveSpeed = Mathf.Clamp(moveSpeed , -Mathf.Infinity , maxMoveSpeed);
-
-        if ( Mathf.Abs(xAxis) > 0.2 || Mathf.Abs(zAxis) > 0.2 ) {
-            lastInput = Time.time;
-            Transform mainCamera = Camera.main.transform;
-
-            Vector3 diff = ( mainCamera.up * zAxis + mainCamera.right * xAxis );
-            diff.Normalize();
-            diff = transform.InverseTransformDirection(diff);
-            diff.y = 0;
-            diff = transform.TransformDirection(diff);
-            transform.LookAt(transform.position + diff , transform.up);
-            lastDiff = diff;
-        }//turn object around withought rotating camera
-
-        Debug.DrawRay(transform.position , lastDiff , Color.blue);
-        moveSpeed = Mathf.Lerp(moveSpeed , 0 , ( Time.time - lastInput ) / slowdownTime); //equivalent to drag
-        if ( !elevating )
-            rb.AddForce(transform.forward * moveSpeed);
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        input = new Vector3();
     }
 
     void FixedUpdate () {
-        addGravity();
+        useGravity();
         processMove();
 
         if ( Input.GetKeyDown("space") )
